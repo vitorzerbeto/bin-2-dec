a0eb4d77fdd4b923940bd606dc252520174da4da
Vitor Zerbeto <vitor.zerbeto@ifood.com.br>
2020-02-27 10:00:11 -0300

    Updates

---
+++ precache-manifest.0916bcc8de4265374166380eec1f5057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90f353c1eeb6835c4d2f151c7be086a",
+    "url": "/bin-2-dec/index.html"
+  },
+  {
+    "revision": "50a86586b2789a8b3455",
+    "url": "/bin-2-dec/static/css/main.cf972369.chunk.css"
+  },
+  {
+    "revision": "d28ad70dcb2045ef10ee",
+    "url": "/bin-2-dec/static/js/2.d8579696.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/bin-2-dec/static/js/2.d8579696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a86586b2789a8b3455",
+    "url": "/bin-2-dec/static/js/main.8165eb6d.chunk.js"
+  },
+  {
+    "revision": "ad1cfb4ca368312d5543",
+    "url": "/bin-2-dec/static/js/runtime-main.e507b4f2.js"
+  }
+]);
